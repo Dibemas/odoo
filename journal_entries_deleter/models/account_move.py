b3521e929ef01e488f85f717388fb080cb54d3a9
MarcoSilva03 <marcoarsilva3@gmail.com>
2025-07-24 08:23:50 +0200

    Add jornal entreis deleter

---
+++ journal_entries_deleter/models/account_move.py
@@ -0,0 +1,19 @@
+from odoo import api, fields, models, _
+import logging
+
+_logger = logging.getLogger(__name__)
+
+
+# python3.10 odoo-bin -c odoo17.conf -d younify_11 -u journal_entries_deleter --dev=all
+
+class AccountMove(models.Model):
+    _inherit = "account.move"
+
+    def action_master_delete_entries(self):
+        for invoice in self:
+            invoice.sudo().write({'state': 'draft'})
+            try:
+                invoice.sudo().unlink()
+                _logger.info(f"Successfully deleted invoice {invoice.id}")
+            except Exception as e:
+                _logger.error(f"Failed to delete invoice {invoice.id}: {e}")
