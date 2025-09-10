7612d5b4eccd34176d7cd16adcfed41340ba0654
MarcoSilva03 <marcoarsilva3@gmail.com>
2025-09-10 09:15:30 +0200

    change from send to _send

--- outgoing_mail_server_bcc/models/mail_mail.py
+++ outgoing_mail_server_bcc/models/mail_mail.py
@@ -6,24 +6,24 @@ class MailMail(models.Model):
 
     is_bcc_copy_sent = fields.Boolean(string='BCC Copy Sent', default=False)
 
-    @api.model
-    def send(self, auto_commit=False, raise_exception=False):
-        result = super(MailMail, self).send(
-            auto_commit=auto_commit, raise_exception=raise_exception)
+    def _send(self, auto_commit=False, raise_exception=False):
+        result = super()._send(auto_commit=auto_commit, raise_exception=raise_exception)
 
         for mail in self:
-            # Check if server has BCC enabled
-            if mail.mail_server_id and mail.mail_server_id.bcc_active and mail.mail_server_id.bcc_email:
-                bcc_email = mail.mail_server_id.bcc_email
-
-                if mail.email_to != bcc_email and not mail.is_bcc_copy_sent:
-                    mail_copy = mail.copy({
-                        'email_to': bcc_email,
-                        'email_cc': False,
-                        'partner_ids': False,
-                        'recipient_ids': False,
-                    })
-                    mail_copy._send(auto_commit=True)
-                    mail.write({'is_bcc_copy_sent': True})
+            if (
+                mail.mail_server_id
+                and mail.mail_server_id.bcc_active
+                and mail.mail_server_id.bcc_email
+                and mail.email_to != mail.mail_server_id.bcc_email
+                and not mail.is_bcc_copy_sent
+            ):
+                mail_copy = mail.copy({
+                    'email_to': mail.mail_server_id.bcc_email,
+                    'email_cc': False,
+                    'partner_ids': False,
+                    'recipient_ids': False,
+                })
+                super(MailMail, mail_copy)._send(auto_commit=True)
+                mail.is_bcc_copy_sent = True
 
         return result
