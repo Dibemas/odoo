389289dc8d37ac5d4e206284c1a5ba1829816ac9
MarcoSilva03 <marcoarsilva3@gmail.com>
2025-09-10 10:51:21 +0200

    Functional bug fix

--- outgoing_mail_server_bcc/models/mail_mail.py
+++ outgoing_mail_server_bcc/models/mail_mail.py
@@ -1,4 +1,7 @@
 from odoo import api, fields, models, _
+import logging
+
+_logger = logging.getLogger(__name__)
 
 
 class MailMail(models.Model):
@@ -17,18 +20,18 @@ class MailMail(models.Model):
         copy=False
     )
 
-    bcc_copy_of_id = fields.Many2one(
-        'mail.mail',
-        string='Original Mail',
-        readonly=True,
-        ondelete='cascade',
-    )
-
     bcc_copy_ids = fields.One2many(
         'mail.mail',
         'bcc_copy_of_id',
         string='BCC Copies',
         readonly=True,
+    )
+
+    bcc_copy_of_id = fields.Many2one(
+        'mail.mail',
+        string='Original Mail',
+        readonly=True,
+        ondelete='cascade',
     )
 
     def _send(self, auto_commit=False, raise_exception=False, smtp_session=None, alias_domain_id=None, mail_server=None, post_send_callback=None,):
@@ -42,28 +45,27 @@ class MailMail(models.Model):
         )
 
         for mail in self:
-            if (
-                mail.mail_server_id
-                and mail.mail_server_id.bcc_active
-                and mail.mail_server_id.bcc_email
-                and mail.email_to != mail.mail_server_id.bcc_email
-                and not mail.is_bcc_copy_sent
-            ):
-                mail_copy = mail.copy({
-                    'email_to': mail.mail_server_id.bcc_email,
-                    'email_cc': False,
-                    'partner_ids': False,
-                    'recipient_ids': False,
-                    'is_bcc_copy': True,
-                    'bcc_copy_of_id': mail.id,
-                })
-                super(MailMail, mail_copy)._send(
-                    auto_commit=True,
-                    smtp_session=smtp_session,
-                    alias_domain_id=alias_domain_id,
-                    mail_server=mail_server,
-                    post_send_callback=post_send_callback,
-                )
-                mail.is_bcc_copy_sent = True
+            if not mail.is_bcc_copy_sent and mail.mail_server_id and mail.mail_server_id.bcc_active and mail.mail_server_id.bcc_email:
+                if mail.email_to != mail.mail_server_id.bcc_email:
+                    mail_copy = mail.copy({
+                        'email_to': mail.mail_server_id.bcc_email,
+                        'email_cc': False,
+                        'partner_ids': [(6, 0, [])],
+                        'recipient_ids': [(6, 0, [])],
+                        'is_bcc_copy': True,
+                        'bcc_copy_of_id': mail.id,
+                    })
+                    try:
+                        mail_copy._send(
+                            auto_commit=True,
+                            smtp_session=smtp_session,
+                            alias_domain_id=alias_domain_id,
+                            mail_server=mail_server,
+                            post_send_callback=post_send_callback,
+                        )
+                    except Exception as e:
+                        _logger.error(
+                            "BCC copy send failed for mail %s: %s", mail.id, e)
+                    mail.is_bcc_copy_sent = True
 
         return result
